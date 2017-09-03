8f5217338e0a930289e0ac454e3928e59844dda7
Fernando Saltoleto <fernandosaltoleto@hotmail.com>
2017-09-03 11:58:30 -0300

    config/express

--- config/express.js
+++ config/express.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var	load	= require('express-load');
+var	bodyParser	=	require('body-parser');
+
 
 module.exports = function(){
 	var app = express();
@@ -11,6 +13,12 @@ module.exports = function(){
 	app.use(express.static('./public'));	
 	app.set('view engine', 'ejs');	
 	app.set('views','./app/views');	
+
+	//	novos	middlewares
+	app.use(bodyParser.urlencoded({extended:	true}));
+	app.use(bodyParser.json());
+	app.use(require('method-override')());
+	
 
 	load('models',	{cwd:	'app'})
 		.then('controllers')
